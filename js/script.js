55aefd215c4a96f6b2161eb2f7fc9e619915d867
VolkovGennadiy <VolkovGennadiy@users.noreply.github.com>
2020-11-05 14:57:56 +0600

    Оживляем интерактивные элементы

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const lostClose = document.querySelector(".modalLost-close");
 const lostAction = document.querySelector(".form-action")
 const lostForm = document.querySelector(".name-form");
 const lostEmail =document.querySelector(".email-form");
+const mapOpen =document.querySelector(".modalMapOpen");
+const mapModal =document.querySelector(".modalMap");
+const mapClose =document.querySelector(".modalMap-close");
 
 lostLink.addEventListener("click", function (evt) { 
   evt.preventDefault();
@@ -29,6 +32,25 @@ window.addEventListener("keydown", function (evt) {
     if (lostPopup.classList.contains("modalLost-show")) {
       evt.preventDefault();
       lostPopup.classList.remove("modalLost-show");
+    }
+  }
+});
+
+mapOpen.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapModal.classList.add("modalMap-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapModal.classList.remove("modalMap-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (mapModal.classList.contains("modalMap-show")) {
+      evt.preventDefault();
+      mapModal.classList.remove("modalMap-show");
     }
   }
 });
